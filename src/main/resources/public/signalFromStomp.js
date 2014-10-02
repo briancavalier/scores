9b813229efcc4313207c83f5f7c41446cdb0fbde
Brian Cavalier <brian@hovercraftstudios.com>
2014-10-02 17:31:46 -0400

    Update comments

--- src/main/resources/public/signalFromStomp.js
+++ src/main/resources/public/signalFromStomp.js
@@ -11,15 +11,13 @@ module.exports = function streamFromStomp(initDestination, updateDestination, st
 			// updates on another.
 
 			// This up-front work will make it easy for consumers to
-			// use the returned stream--it will look like a homogenous
-			// stream of JSON Patches.
+			// use the returned stream--it will look like a homogeneous
+			// stream of scores.
 
-			// A most-stomp add-on could help here.
-
-			// Create a stream containing the one and only initial copy
-			// of the full data, transformed into a replace-whole-document
-			// so that consumers of the outer stream (returned above)
-			// get a homogeneous stream of patches.
+			// Create a stream containing one full copy of the data, and
+			// flatMap that to a stream containing the time-varying
+			// current set of scores, using scan to accumulate each patch
+			// and emit the updated data.
 			var signal = fromStompJson(initDestination, stomp)
 				.take(1)
 				.flatMap(function(data) {
@@ -32,14 +30,15 @@ module.exports = function streamFromStomp(initDestination, updateDestination, st
 
 			add(signal);
 
-			// We're creating a higher-order stream.
-			// The outer stream contains one event: an inner stream of patches
-			// We use join() below to flatten the higher-order stream to
-			// a first-order stream of patches.
 		});
 
 		// Return a dispose function for the outer stream;
 		return stomp.disconnect.bind(stomp);
+
+		// We've created a higher-order stream.
+		// The outer stream contains one event: an inner stream of scores.
+		// We use join() below to flatten the higher-order stream to
+		// a first-order stream of time-varying score data.
 
 	}).join();
 };
